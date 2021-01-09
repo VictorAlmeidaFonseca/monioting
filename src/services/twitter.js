b684d89d54f6e1ddb00157fc06275c4279ed9505
VictorAlmeidaFonseca <fonseca.almeida.victor@gmail.com>
2021-01-09 00:42:06 -0300

    create tweets service: get-by-hashTag

--- src/services/twitter.js
+++ src/services/twitter.js
@@ -1,13 +1,22 @@
-const axios = require('axios')
+require('dotenv').config()
+const hasCharacter = require('../utils/has-characters')
+const twitter = require('../config/twitter-connection')
 
-const twitter = axios.create({
-    baseURL: 'https://api.twitter.com/2/',
-    timeout: 1000,
-    headers: {
-        'Authorization': `Bearer ${process.env.TWITTER_BAERER_TOKEN}`,
-        'Access-Control-Request-Headers': 'authorization',
-        'Access-Control-Allow-Headers' : '*/*'
+
+async function getTweetsByHashtag(query) {
+
+    const params = { query,  max_results: 100 }
+
+    try {        
+        const hasHashtag = hasCharacter(query, '#')        
+        if(!hasHashtag) throw `query must be a hashtag!`
+
+        const response = await twitter.get('tweets/search/recent', { params }) 
+        return response
+
+    } catch (error) {
+        throw error
     }
-});
+}
 
-module.exports = twitter
+module.exports = { getTweetsByHashtag }
---
+++ test/get-tweet-by-hashtag.test.js
@@ -0,0 +1,21 @@
+const { getTweetsByHashtag } = require('../src/services/twitter')
+
+const mockHashtag = "#sre"
+
+describe(`Twitter's services`, () => {
+
+    it('should get twitter only by hashtag', async () => {
+      const response = await getTweetsByHashtag(mockHashtag)
+      expect(response.status).toEqual(200)
+      expect(response.data.data.some(data => data.text.includes(mockHashtag))).toBe(true)     
+    })
+
+    it('should throw erro: not hashtag', async () => {
+        try {
+          const response = await getTweetsByHashtag('')
+         } catch (error) {
+            expect(error).toEqual("query must be a hashtag!")   
+        }
+    })
+  
+})
