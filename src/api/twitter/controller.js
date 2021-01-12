8cc3f93d70c8af5196f57b523f0f7337a25ff135
Victor Almeida <36797751+VictorAlmeidaFonseca@users.noreply.github.com>
2021-01-12 00:19:16 -0300

    Update controller.js

--- src/api/twitter/controller.js
+++ src/api/twitter/controller.js
@@ -3,7 +3,7 @@ const { find } = require('../../dao/twitterDAO')
 async function getTopFollowers(req, res) {
     try {
         const limit = req.params.limit ? req.params.limit : 5
-        const sort = { length: -1 }
+        const sort = { followersNumber: -1 }
         const query = { limit, sort}     
         const getUsersHasMoreFollowers = await find(query)
 
